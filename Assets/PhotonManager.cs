650569b76590587f078992515a0960569add4bed
nuvasuper <cdkrusemark@gmail.com>
2015-03-06 12:50:04 -0800

    Revert "Character select!"
    This reverts commit b1f675c4d3b497be7798878e084c5be2c7f4dba0.

--- Assets/PhotonManager.cs
+++ Assets/PhotonManager.cs
@@ -6,7 +6,6 @@ public class PhotonManager : MonoBehaviour {
 	public string playerPrefab;
 	public GameObject standbyCamera;
 	Spawnpoint[] spawnpoints;
-	public GameObject cS;
 	// Use this for initialization
 	void Start () {
 		Connect ();
@@ -33,32 +32,16 @@ public class PhotonManager : MonoBehaviour {
 	void OnJoinedRoom() {
 		spawnpoints = GameObject.FindObjectsOfType<Spawnpoint> ();
 		Debug.Log ("OnJoinedRoom");
-		chooseCharacter ();
-
-	}
-
-	void chooseCharacter() {
-		cS.SetActive (true);
-	}
-
-	public void pickPrefab(string newPrefab) {
-		playerPrefab = newPrefab;
-		cS.SetActive (false);
 		SpawnMyPlayer ();
 	}
-
 	void SpawnMyPlayer() {
 		Spawnpoint mySpawnpoint = spawnpoints [Random.Range(0, spawnpoints.Length)];
-		GameObject myPlayerGO;
-		if (playerPrefab.Equals("Chair")||playerPrefab.Equals("Crate")||playerPrefab.Equals("Table")) {
-			myPlayerGO = (GameObject)PhotonNetwork.Instantiate (playerPrefab, mySpawnpoint.transform.position, Quaternion.Euler(-90,0,0), 0);
-		} else {
-			myPlayerGO = (GameObject)PhotonNetwork.Instantiate (playerPrefab, mySpawnpoint.transform.position, Quaternion.Euler(0,0,0), 0);
-		}
+
+		GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate (playerPrefab, mySpawnpoint.transform.position, Quaternion.Euler(-90,0,0), 0);
 		standbyCamera.SetActive (false);
 		myPlayerGO.GetComponent<Move> ().enabled = true;
 		myPlayerGO.GetComponent<Player> ().enabled = true;
-		//myPlayerGO.GetComponent<Chair> ().enabled = true;
+		myPlayerGO.GetComponent<Chair> ().enabled = true;
 		myPlayerGO.transform.FindChild("Dummy").gameObject.SetActive (true);
 	}
 }
